b594461e0c32895284532f67c146a09f2b1b724f
Vaishali parmar <113784660+Vaishaliparmar01@users.noreply.github.com>
2023-10-06 13:37:37 +0530

    modified code

--- src/components/Forthsec.tsx
+++ src/components/Forthsec.tsx
@@ -86,13 +86,13 @@ function Forthsec() {
 
             </div>
             <br />
-<div className="flex  hover:scale-120 transition-transform duration-300 ease-in-out">
-<svg className="h-10 w-10 text-blue-400"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<div className="flex ">
+<svg className="h-10 w-10 text-blue-400 "  fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
 </svg> 
             <a
               href="#"
-              className="lg:text-4xl text-blue-600 dark:text-blue-500  hover:underline"
+              className="lg:text-4xl text-blue-600 dark:text-blue-500 hover:scale-150 transition-transform duration-300 ease-in-out  "
               data-aos="fade-right"
             >
               +9177633776 
